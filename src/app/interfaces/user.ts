01d9fdf33d516a732f643e09deca2d2aafe6b2ee
CatalinaC16 <catalina.ciochina16@gmail.com>
2023-11-05 13:06:28 +0200

    First try

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  name: string;
+  familyName: string;
+  email: string;
+  password : string;
+  imageUrl : string;
+  jobTitle : string;
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../interfaces/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService{
+  private apiServiceUrl = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public addUser(user : User):Observable<User>{
+    return this.http.post<User>(`${this.apiServiceUrl}/user/add`, user);
+  }
+
+  public getUsers():Observable<User[]>{
+    return this.http.get<User[]>(`${this.apiServiceUrl}/user/all`);
+  }
+
+  public updateUser(user : User):Observable<User>{
+    return this.http.put<User>(`${this.apiServiceUrl}/user/update`, user);
+  }
+
+  public getUserById(userId : number):Observable<User>{
+    return this.http.get<User>(`${this.apiServiceUrl}/user/find/${userId}`);
+  }
+
+  public deleteUser(userId : number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServiceUrl}/user/delete/${userId}`);
+  }
+
+
+}
