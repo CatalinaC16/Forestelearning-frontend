d310c8d6fa33e9c2284702d9217b36ec8cc35fdc
CatalinaC16 <catalina.ciochina16@gmail.com>
2023-12-24 18:08:01 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { BodyComponent } from './parts/body/body.component';
-import { SideNavComponent } from './parts/side-nav/side-nav.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { PracticeComponent } from './pages/practice/practice.component';
@@ -15,6 +13,10 @@ import { HelpUserComponent } from './pages/help-user/help-user.component';
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {SideNavComponent} from "./components/side-nav/side-nav.component";
+import {BodyComponent} from "./components/body/body.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     GradesComponent,
     AwardsComponent,
     HelpUserComponent,
-    SideNavComponent
+    SideNavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {SideNavToggle} from "../../interfaces/SideNavToggle";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent{
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
+
+  getHeaderClass() {
+    let cls = '';
+    if(this.collapsed && this.screenWidth>768){
+      cls = 'header-trimmed';
+    }
+    else if(this.collapsed && this.screenWidth<=768 && this.screenWidth>0 )
+    {
+      cls ='header-screen';
+    }
+    return cls;
+  }
+}
