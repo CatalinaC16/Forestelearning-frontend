8f2a85251064d4b960ae9dbbd6761561d1658b27
CatalinaC16 <catalina.ciochina16@gmail.com>
2023-12-27 16:03:05 +0200

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +6,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent{
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
 
+  getFooterClass() {
+    let cls = '';
+    if(this.collapsed && this.screenWidth>768){
+      cls = 'footer-trimmed';
+    }
+    else if(this.collapsed && this.screenWidth<=768 && this.screenWidth>0 )
+    {
+      cls ='footer-screen';
+    }
+    return cls;
+  }
 
 }
